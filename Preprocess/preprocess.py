831891f36ff8c959fd2580b824ca0f20bc8bade6
Master2204OR <108833716+Master2204OR@users.noreply.github.com>
2025-06-19 02:51:56 +0200

    m

--- Preprocess/preprocess.py
+++ Preprocess/preprocess.py
@@ -4,7 +4,7 @@ from sklearn import train_test_split, RobustScaler, shuffle
 
 
 
-def preprocess_data(file = 'data/creditcard.csv', test_data = 0.2):
+def preprocess_data(file = 'data/creditcard.csv', test_size = 0.2,  random_state=42):
 
     data = pd.read_csv(file)
 
@@ -17,71 +17,60 @@ def preprocess_data(file = 'data/creditcard.csv', test_data = 0.2):
     data.drop(columns=['Time'], inplace=True)
     print("Dropped 'Time' column as it is not needed.")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def load_credit_card_data(filepath='data/creditcard.csv', test_size=0.2, random_state=42):
-    df = pd.read_csv(filepath)
-
-    before = df.shape[0]
-    df.drop_duplicates(inplace=True)
-    after = df.shape[0]
-    print(f"Removed {before - after} duplicate rows.")
-
-    
-    df.fillna(df.mean(numeric_only=True), inplace=True)
-
-    if 'Time' in df.columns:
-        df.drop(columns=['Time'], inplace=True)
-
     scaler = RobustScaler()
-    df['Amount'] = scaler.fit_transform(df[['Amount']])
+    data['Amount'] = scaler.fit_transform(data['Amount'])
 
-    X = df.drop(columns=['Class']).values
-    y = df['Class'].values
+    X = data.drop(columns=['Class']).values
+    y = data['Class'].values
+
+    X = data.drop(columns=['Class']).values
+    y = data['Class'].values
 
     X, y = shuffle(X, y, random_state=random_state)
 
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=test_size, stratify=y, random_state=random_state
-    )
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, stratify=y, random_state=random_state)
 
     return (X_train, y_train), (X_test, y_test)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
